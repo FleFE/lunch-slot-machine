ca1441bfd767b42a392968058a15f4825b601298
高凌霄 <gaolx@gaolingxiaodeMac-Studio.local>
2023-06-13 09:46:44 +0800

    ci

--- pages/api/getmenu.js
+++ pages/api/getmenu.js
@@ -1,12 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const fs = require("fs");
 const path = require("path");
+let tempDate = null;
 export default function handler(req, res) {
-  const file = fs.readFileSync(
-    path.join(path.join(process.cwd(), "localdata"), "menu.json"),
-    {
-      encoding: "utf8",
-    }
-  );
+  if (tempDate === null) {
+    tempDate = fs.readFileSync(
+      path.join(path.join(process.cwd(), "localdata"), "menu.json"),
+      {
+        encoding: "utf8",
+      }
+    );
+  }
   res.status(200).json(JSON.parse(file));
 }
